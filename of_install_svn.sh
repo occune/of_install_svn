e9923c202a2cb9c1cadf89bfa73afb643e6ba572
occune <batyamedic@gmail.com>
2019-11-23 12:25:43 +0300

    Update of_install_svn.sh

--- of_install_svn.sh
+++ of_install_svn.sh
@@ -24,5 +24,5 @@ if [ -d "open_fortress" ]; then
     cd ..
 else
    echo "Starting the download of a new copy"
-   svn checkout --username ofs --password '' --depth infinity http://svn.openfortress.fun/open_fortress/
+   svn checkout --username ofs --password '' --depth infinity https://svn.openfortress.fun/svn/open_fortress
 fi
